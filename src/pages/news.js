0b2026303accf44a7529070141e9c2127cd03887
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-08-02 16:46:25 +0900

    演習

--- src/pages/news.js
+++ src/pages/news.js
@@ -4,10 +4,8 @@ import style from  "../styles/test.module.scss"
 const news = () => {
   return
     <div className="text-5xl underline">
-      news
       <p className={style.test}>テスト</p>
     </div>
-  )
 }
 
 export default news
